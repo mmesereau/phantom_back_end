b9534e005501404372eb1100dbf59ac8e8e7ecea
Michael Mesereau <mmesereau1@gmail.com>
2016-09-19 13:04:34 -0600

    updating seeds to change passwords to hashes

--- seeds/users.js
+++ seeds/users.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('users').insert({id: 1, username: 'Mike1', password: '12345', nickname: 'Mike', wins: 7, losses: 2}),
-        knex('users').insert({id: 2, username: 'Mike2', password: '12345', nickname: 'Michael', wins: 2, losses: 7}),
-        knex('users').insert({id: 3, username: 'Mike3', password: '12345', nickname: 'Mikey', wins: 0, losses: 9})
+        knex('users').insert({id: 1, username: 'Mike1', password: '$2a$08$jLYMPrF0V7oxbBw2ucBdcOFQBP/69tCYSlRRyOSapwMRpTtQ5BmYa', nickname: 'Mike', wins: 7, losses: 2}),
+        knex('users').insert({id: 2, username: 'Mike2', password: '$2a$08$jLYMPrF0V7oxbBw2ucBdcOFQBP/69tCYSlRRyOSapwMRpTtQ5BmYa', nickname: 'Michael', wins: 2, losses: 7}),
+        knex('users').insert({id: 3, username: 'Mike3', password: '$2a$08$jLYMPrF0V7oxbBw2ucBdcOFQBP/69tCYSlRRyOSapwMRpTtQ5BmYa', nickname: 'Mikey', wins: 0, losses: 9})
       ]);
     });
 };
