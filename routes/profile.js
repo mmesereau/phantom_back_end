c4df6576d02ed3066af4d7907dcb028a930aa5db
Michael Mesereau <mmesereau1@gmail.com>
2016-09-20 11:10:08 -0600

    what is req dot user

--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 var knex = require('../db/knex');
 
 router.get('/', function(req, res) {
+  console.log(req.user);
   knex('users').where('id', req.user.id)
   .then(function(data) {
     res.send(data);
