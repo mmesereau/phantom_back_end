657b7f87162cb65ce17ae6a63e702906ecc0ce23
Michael Mesereau <mmesereau1@gmail.com>
2016-09-27 15:17:23 -0600

    remove seeds

--- seeds/users.js
+++ seeds/users.js
@@ -1,13 +1,5 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users').del()
-  .then(function () {
-    return Promise.all([
-      // Inserts seed entries
-      knex('users').insert({id: 1, username: 'Mike1', nickname: 'Mike1', wins: 0, losses: 0}),
-      knex('users').insert({id: 2, username: 'Mike2', nickname: 'Mike2', wins: 0, losses: 0}),
-      knex('users').insert({id: 3, username: 'Mike3', nickname: 'Mike3', wins: 0, losses: 0})
-    ]);
-  });
+  return knex('users').del();
 };
