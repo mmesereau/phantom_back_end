e33218d2a101c82d5dea44153355d0d4346bf17d
Michael Mesereau <mmesereau1@gmail.com>
2016-09-27 10:01:13 -0600

    win loss

--- routes/loss.js
+++ routes/loss.js
@@ -2,13 +2,29 @@
 var router = require('express').Router();
 var knex = require('../db/knex');
 
-router.put('/', function(req, res) {
-  knex('users').where('id', req.params.id)
+// router.put('/', function(req, res) {
+//   knex('users').where('id', req.params.id)
+//   .then(function(data) {
+//     if (data[0]) {
+//       data[0].losses += 1;
+//     }
+//     return knex('users').where('id', req.params.id).update(data[0]);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+// });
+
+router.post('/', function(req, res) {
+  knex('users').where('nickname', req.body.nickname)
   .then(function(data) {
     if (data[0]) {
-      data[0].losses += 1;
+      data[0].losses++;
+      return knex('users').where('nickname', req.body.nickname).update(data[0]);
     }
-    return knex('users').where('id', req.params.id).update(data[0]);
+    else {
+      return knex('users').insert({nickname: req.body.nickname, wins: 0, losses: 1});
+    }
   })
   .catch(function(err) {
     console.log(err);
--- routes/win.js
+++ routes/win.js
@@ -2,13 +2,29 @@
 var router = require('express').Router();
 var knex = require('../db/knex');
 
-router.put('/', function(req, res) {
-  knex('users').where('id', req.params.id)
+// router.put('/', function(req, res) {
+//   knex('users').where('id', req.params.id)
+//   .then(function(data) {
+//     if (data[0]) {
+//       data[0].wins += 1;
+//     }
+//     return knex('users').where('id', req.params.id).update(data[0]);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+// });
+
+router.post('/', function(req, res) {
+  knex('users').where('nickname', req.body.nickname)
   .then(function(data) {
     if (data[0]) {
-      data[0].wins += 1;
+      data[0].wins++;
+      return knex('users').where('nickname', req.body.nickname).update(data[0]);
     }
-    return knex('users').where('id', req.params.id).update(data[0]);
+    else {
+      return knex('users').insert({nickname: req.body.nickname, wins: 1, losses: 0});
+    }
   })
   .catch(function(err) {
     console.log(err);
